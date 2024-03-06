34e32ff6fef338190c4328d87477feae6834ee9f
fmads <fmads@BRUGER-PC>
2024-03-06 09:47:55 +0100

    Add project files.

---
+++ ActorRepositoryLib/IActorsRepository.cs
@@ -0,0 +1,15 @@
+﻿
+namespace ActorRepositoryLib
+{
+    public interface IActorsRepository
+    {
+        Actor Add(Actor newActor);
+        Actor? Delete(int Id);
+        IEnumerable<Actor> Get(string? nameFilter,
+            int? minBirthYear,
+            int? maxBirthYear,
+            int? amount);
+        Actor? GetById(int id);
+        Actor? Update(int Id, Actor updates);
+    }
+}
---
+++ ActorRepositoryLib/Secrets.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActorRepositoryLib
+{
+    public class Secrets
+    {
+        public const string ConnectionString = 
+            "server=localhost;database=MovieDB;user id=MovieMan;" +
+            "password=MovieMan;TrustServerCertificate=True";
+    }
+}
